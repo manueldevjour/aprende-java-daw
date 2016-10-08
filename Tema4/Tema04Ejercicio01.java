f0deaf4049fb5ee100c8d1daa5b926703a93ddbb
Manuel Cerezo Bonilla <manolocb96@gmail.com>
2016-10-08 19:52:04 +0200

    Ejercicios 1 y 2 añadidos, falta por añadir el numero 4

---
+++ Tema4/Tema04Ejercicio01.java
@@ -0,0 +1,33 @@
+/**
+ *
+ *
+ * Escribe un programa que pida por teclado un día de la semana y que diga qué
+asignatura toca a primera hora ese día.
+ *
+ *
+ * @author Manuel Cerezo
+ *
+ */
+ 
+public class Tema04Ejercicio01 {
+	public static void main(String[] args) {
+		
+		System.out.println("Escriba el dia de la semana para conocer la asignatura que tiene a primera hora:");
+		String respuesta = System.console().readLine();
+		
+		if (respuesta.toUpperCase().equals("LUNES")) {
+			System.out.println("Sistemas informaticos");
+		} else if (respuesta.toUpperCase().equals("MARTES")) {
+			System.out.println("Programacion");
+		} else if (respuesta.toUpperCase().equals("MIERCOLES")) {
+			System.out.println("Programacion");
+		} else if (respuesta.toUpperCase().equals("JUEVES")) {
+			System.out.println("Bases de datos");
+		} else if (respuesta.toUpperCase().equals("VIERNES")) {
+			System.out.println("Lenguaje de marcas");
+		} else {
+			System.out.println("Ese día de la semana no existe");
+		}
+
+	}
+}		
