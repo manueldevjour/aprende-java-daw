a7b14a5c1f37a687b7eae1be1583fbb8580f8f76
Manuel Cerezo Bonilla <manolocb96@gmail.com>
2016-10-20 13:44:26 +0200

    Ejercicio 17 actualizado con Math.abs

--- Tema4/Tema04Ejercicio17.java
+++ Tema4/Tema04Ejercicio17.java
@@ -11,10 +11,9 @@ public class Tema04Ejercicio17 {
   public static void main(String[] args) {
   
     System.out.print("Introduzca un número: ");
-    int numero = Integer.parseInt(System.console().readLine());
-    
-    int resultado = (numero % 10);
-    System.out.println("La última cifra del numero introducido es: " + resultado);
+    int numero = Math.abs(Integer.parseInt(System.console().readLine()));
+
+    System.out.println("La última cifra del numero introducido es: " + numero % 10);
     
   }
 }
